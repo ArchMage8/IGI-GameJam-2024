bcee499cfe7abc54482d2219a5857c1c1051f6ce
Arvin Sadeli <darryl.orleans@binus.ac.id>
2024-08-03 16:43:28 +0700

    ship repair

---
+++ Assets/Scripts/FixShip.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixShip : MonoBehaviour
+{
+    private bool canFix = true;
+   
+    private Color targetColor = new Color(0.435f, 0.435f, 0.435f); // 6F6F6F in RGB
+    private SpriteRenderer spriteRenderer;
+
+    [Header("System")]
+    public float delay = 2f;
+    public int healing = 0;
+    public HealthSystem shipHealth;
+    public GetMaterials materials;
+    [Space(30)]
+
+    [Header("MaterialCost")]
+    public int SteelCost;
+    public int ElectronicCost;
+    public int PlasticCost;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canFix)
+        {
+            canFix = false;
+            spriteRenderer.color = targetColor;
+            StartCoroutine(ResetBoolAfterDelay());
+            Fixing();
+        }
+    }
+
+    private IEnumerator ResetBoolAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        canFix = true;
+    }
+
+    private void Fixing()
+    {
+        materials.CollectedSteel -= SteelCost;
+        materials.CollectedPlastic -= PlasticCost;
+        materials.CollectedElectronic -= ElectronicCost;
+
+        shipHealth.Health += healing;
+    }
+}
